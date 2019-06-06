74e79d8b29358913b935623a66ca050ead091e8f
Anika <info@anikaszuppa.de>
2019-06-06 13:28:05 +0000

    Fill constants again

--- src/components/App/constants.js
+++ src/components/App/constants.js
@@ -1,3 +1,4 @@
+// your API settings here
 export const API_POST_URL = 'http://34.243.3.31:8080';
 export const API_GET_URL = 'http://34.243.3.31:8080/channels';
 export const API_TOKEN = { 'X-Group-Token': 'glvWcuOo6Rl7' };
