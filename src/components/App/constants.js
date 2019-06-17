641b8000eaf2c88760fe114c7e74ca41e1f6caad
Anika <info@anikaszuppa.de>
2019-06-17 12:12:40 +0000

    Add credentials

--- src/components/App/constants.js
+++ src/components/App/constants.js
@@ -1,7 +1,7 @@
 // api settings
-export const API_POST_URL = '';
-export const API_GET_URL = '';
-export const API_TOKEN = { '': '' };
+export const API_POST_URL = 'http://34.243.3.31:8080';
+export const API_GET_URL = 'http://34.243.3.31:8080';
+export const API_TOKEN = { 'X-Group-Token': 'glvWcuOo6Rl7' };
 export const API_HEADERS = {
     'Content-Type': 'application/json',
     ...API_TOKEN
