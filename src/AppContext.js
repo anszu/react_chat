5dde8c8f8a71d3a6f8ee7a032fd37c042371da4e
Anika <info@anikaszuppa.de>
2019-08-31 10:31:08 +0000

    Delete not needed context object

--- src/AppContext.js
+++ src/AppContext.js
@@ -3,4 +3,3 @@ import React from "react";
 // create context objects
 export const AppContext = React.createContext({});
 export const AppContextProvider = AppContext.Provider;
-export const AppContextConsumer = AppContext.Consumer;
